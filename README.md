eaff897bc1ed096099d0460755fe0ad3020f3572
IsaiasMorochi <isaiasmorochi95@gmail.com>
2019-08-03 19:10:22 -0400

    refactor de conversion de decenas y unidades

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 * Metodología de programación agnostico al lenguaje de programación.
 * Programación diseñada por test.
 
-### Ciclo
+### Ciclo de vida de TDD
 ![alt text](https://drupal.ed.team/sites/default/files/2018-12/Ciclo%20de%20vida%20chiqui_4.png "Logo Title Text 1")
 
 ### Ventajas
--- src/main/java/NumerosRomanos.java
+++ src/main/java/NumerosRomanos.java
@@ -1,75 +1,65 @@
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author imorochi
  */
 public class NumerosRomanos {
 
+    private static final List<String> simbolosRomanos = Arrays.asList("I", "V", "X", "L", "C");
+
     public String convertirRomanos(Integer numeroNatural) {
-
         char[] numerosChart = numeroNatural.toString().toCharArray();
-
         if (numerosChart.length >= 2){
-            String unidadRomana = pasarUnidad(Character.getNumericValue(numerosChart[1]));
-            String decenaRomana = pasarDecena(Character.getNumericValue(numerosChart[0]));
+            String unidadRomana = generico(Character.getNumericValue(numerosChart[1]),0,1,2);
+            String decenaRomana = generico(Character.getNumericValue(numerosChart[0]),2,3,4);
             return decenaRomana + unidadRomana;
         }
-
         if (numerosChart.length >= 1)
-            return pasarUnidad(Character.getNumericValue(numerosChart[0]));
+            return generico(Character.getNumericValue(numerosChart[0]), 0, 1, 2);
 
         return null;
     }
 
-    private String pasarDecena(int decena) {
+    private String generico(int decena, int x, int y, int z) {
         switch (decena){
             case 4:
-                return "XL";
+                return simbolosRomanos.get(x) + simbolosRomanos.get(y);
             case 9:
-                return "XC";
+                return simbolosRomanos.get(x) + simbolosRomanos.get(z);
         }
-
         if (decena <= 3)
-            return sumarX(1, decena,"");
-
+            return sumar(1, decena,"", simbolosRomanos.get(x));
         if (decena <= 8)
-            return sumarX(6, decena,"L");
-
+            return sumar(6, decena,simbolosRomanos.get(y), simbolosRomanos.get(x));
         return null;
     }
-
-    private String pasarUnidad(int unidad) {
-        switch (unidad){
-            case 4:
-                return "IV";
-            case 9:
-                return "IX";
-        }
-
-        if (unidad <= 3)
-            return sumarI(1,unidad,"");
-
-        if (unidad <= 8)
-            return sumarI(6,unidad,"V");
-
-        return null;
-    }
-
-//    private String sumarI(int numeroNatural){
-//        String resultado = "";
-//        for (int i = 1; i <= numeroNatural; i++){
-//            resultado += "I";
-//        }
-//        return resultado;
-//    }
-//
-//    private String sumarIAPartirV(int numeroNatural){
-//        String resultado = "V";
-//        for (int i = 6; i <= numeroNatural; i++){
-//            resultado += "I";
-//        }
-//        return resultado;
-//    }
 
     //refactor
+    private String sumar(int inicioCenta, int numeroNatural, String numeroHermano, String incRomano){
+        for (int i = inicioCenta; i <= numeroNatural; i++){
+            numeroHermano += incRomano;
+        }
+        return numeroHermano;
+    }
+
+    /*
+    private String sumarI(int numeroNatural){
+        String resultado = "";
+        for (int i = 1; i <= numeroNatural; i++){
+            resultado += "I";
+        }
+        return resultado;
+    }
+
+    private String sumarIAPartirV(int numeroNatural){
+        String resultado = "V";
+        for (int i = 6; i <= numeroNatural; i++){
+            resultado += "I";
+        }
+        return resultado;
+    }
+
     private String sumarI(int inicioCenta, int numeroNatural, String numeroHermano){
         for (int i = inicioCenta; i <= numeroNatural; i++){
             numeroHermano += "I";
@@ -83,4 +73,32 @@ public class NumerosRomanos {
         }
         return numeroHermano;
     }
+
+    private String pasarDecena(int decena) {
+        switch (decena){
+            case 4:
+                return "XL";
+            case 9:
+                return "XC";
+        }
+        if (decena <= 3)
+            return sumar(1, decena,"","X");
+        if (decena <= 8)
+            return sumar(6, decena,"L","X");
+        return null;
+    }
+
+    private String pasarUnidad(int unidad) {
+        switch (unidad){
+            case 4:
+                return "IV";
+            case 9:
+                return "IX";
+        }
+        if (unidad <= 3)
+            return sumar(1,unidad,"","I");
+        if (unidad <= 8)
+            return sumar(6,unidad,"V","I");
+        return null;
+    }*/
 }
